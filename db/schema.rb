d805ca4e01b7af5ed32171311d1187c64934a9dd
3171km <kyusemasahiro@gmail.com>
2023-06-15 15:08:02 +0900

    Renderでデプロイ2

--- db/schema.rb
+++ db/schema.rb
@@ -72,4 +72,5 @@ ActiveRecord::Schema.define(version: 2023_06_14_053842) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "prototypes"
   add_foreign_key "comments", "users"
+  add_foreign_key "prototypes", "users"
 end
